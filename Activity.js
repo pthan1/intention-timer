6f2205ce7a8afd753d1ba0342328d017b1c5a024
Nate Sheridan <nbs@dr.com>
2021-07-28 19:14:11 -0600

    Implement functional timer

--- Activity.js
+++ Activity.js
@@ -1,23 +1,52 @@
 class Activity{
-    constructor(){
+    constructor(category, description, minutes, seconds){
         this.category = category;
         this.description = description;
         this.minutes = minutes;
         this.seconds = seconds;
         this.completed = false;
-        this.id = Date.now();
+        this.id = `ID${Date.now()}`;
+        // this.countdownTimeInMS = (this.minutes*60000)+(this.seconds*1000);
+        // this.duration = countdownTimeInMS/1000
+        this.running = false
     }
 
-    countdown(){
-        //we need to get the total time in miliseconds of this.minutes and this.seconds together
-        var countdownTimeMS = this.minutes
-        var stopTime = 
+    startTimer(){
+        if(this.running){
+            return;
+        }
+        this.running = true;
+        var seconds = this.seconds;
+        var minutes = this.minutes;
+        var self = this;
+        var interval = setInterval(function(){
+            console.log(minutes+" " + seconds);
+            // document.querySelector("#timer").innerHTML = minute + ":" + seconds;
+            seconds--;
+            if (seconds == 0){
+                minutes--;
+                seconds = 60;
+                if(minutes == -1){
+                    clearInterval(interval);
+                    self.markComplete()
+                    return ""
+                    // document.querySelector("#timer").innerHTML = 'Complete!'
+                }
+            }
+        }, 1000) //run through the setInterval function every 1000 milliseconds
+
+
     }
     markComplete(){
-
+        
+        console.log("timer complete")
     }
     saveToStorage(){
 
+    }
+
+    parseTime(){
+        
     }
 
 }
