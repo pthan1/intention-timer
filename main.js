f312b4511d86fd5f363b8c3265cf37c839dcf647
Anna Johnson <ae.johnson2931@gmail.com>
2021-07-29 15:32:24 -0700

    Add rough start of form functionality for iteration 2

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+var submitForm = document.querySelector(".activity-button");
+var formView = document.querySelector(".left-section");
+var timerView = document.querySelector(".timer-container");
+var activityInput = document.querySelector(".text-field-long");
+var timeInput = document.querySelector(".time-inputs-sections")
+var categoryName = document.querySelector(".category-name");
 
+submitForm.addEventListener("click", submitActivity);
+
+
+function submitActivity(event) {
+  event.preventDefault();
+  formView.classList.add("hidden");
+  timerView.classList.remove("hidden");
+  var categoryChoice = document.querySelector('input[name="activity_categories"]:checked');
+  if (categoryChoice.value === "study") {
+    categoryName.innerText = "Study";
+  } if (categoryChoice.value === "meditate") {
+    categoryName.innerText = "Meditate";
+  } if (categoryChoice.value === "exercise")
+    categoryName.innerText = "Exercise";
+
+}
