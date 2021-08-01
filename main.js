7845b2678393e550b990d62806e3532f45efd43c
Anna Johnson <ae.johnson2931@gmail.com>
2021-08-01 14:25:56 -0700

    Delete commented out code

--- main.js
+++ main.js
@@ -43,10 +43,8 @@ function submitActivity(event) {
   categoryName.innerText = activityInput.value;
   activityCards.push(newCard);
 }
-// leftSection.addEventListener('click', categoryChoice2.startTimer);
 
 function onlyNumberKey(evt) {
-  // Only ASCII character in that range allowed
   var ASCIICode = (evt.which) ? evt.which : evt.keyCode
   if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
       return false;
